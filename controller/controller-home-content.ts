ce23b772c0c1d218db5aa3718d75a32bd641b8d1
pozaka <e41211247@student.polije.ac.id>
2022-12-03 08:10:48 +0700

    updatehomecontent

--- controller/controller-home-content.ts
+++ controller/controller-home-content.ts
@@ -1,5 +1,5 @@
 import http from "http";
-
+import bcrypt from "bcrypt";
 import SQLConnection from "../config/sql-connection";
 import AuthAccessToken from "../utils/auth-access-token";
 import RestAPIFormat from "../utils/rest-api-format";
@@ -44,6 +44,68 @@ class homeRoute {
         res.writeHead(404);
         res.end(JSON.stringify(RestAPIFormat.status404("User not found")));
         }
+    }
+
+    public static async UpdatehomeContent(
+        req: http.IncomingMessage,
+        res: http.ServerResponse
+    ) {
+        let requestBody: string;
+
+        req.on("data", async (chunk) => {
+        requestBody = chunk;
+        });
+
+        req.on("end", () => {
+        const { email, password } = JSON.parse(requestBody);
+        connection
+            .select(`SELECT * FROM user WHERE email=? `, [email])
+            .catch((err) => {
+            console.log(err);
+            })
+            .then((chunk) => {
+            const verifyPassword = bcrypt.compareSync(password, chunk.password);
+
+            const token = AuthAccessToken.createAccessToken({
+                email: chunk.email,
+                username: chunk.username,
+            });
+
+            connection.update(`UPDATE user SET cookies=? WHERE email=?`, [
+                token,
+                chunk.email,
+            ]);
+
+            if (verifyPassword) {
+                const result = JSON.stringify(
+                RestAPIFormat.status200(
+                    {
+                    userId: chunk.user_id,
+                    email: chunk.email,
+                    username: chunk.username,
+                    phone: chunk.hp,
+                    address: chunk.id_address,
+                    image: chunk.img,
+                    token: token,
+                    balance: chunk.saldo,
+                    create_at: chunk.create_at,
+                    update_at: chunk.update_at,
+                    },
+                    "Login success"
+                )
+                );
+                res.writeHead(200);
+                res.end(result);
+            } else {
+                res.writeHead(401);
+                res.end(
+                JSON.stringify(
+                    RestAPIFormat.status401("Password not match")
+                )
+                );
+            }
+            });
+        });
     }
 
 
