7966ffd63aa56348978d7858a67f2a2f6a2bf601
okifirsyah404 <okifirsyah404@gmail.com>
2022-12-16 06:55:54 +0700

    update api

--- controller/controller-order-on-site.ts
+++ controller/controller-order-on-site.ts
@@ -6,111 +6,292 @@ import SQLConnection from "../config/sql-connection";
 import RestAPIFormat from "../utils/rest-api-format";
 import AuthAccessToken from "../utils/auth-access-token";
 import ParseJSON from "../utils/json-parse";
+import bcrypt from "bcrypt";
 
 const connection = SQLConnection.getInstance();
 connection.getConnection();
 
 class OrderOnSiteRoute {
+  public static async getOrderOnSiteData(
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
+    const token = req.headers.authorization;
+
+    try {
+      if (token) {
+        let requestBody: string;
+
+        req.on("data", async (chunk) => {
+          requestBody = ParseJSON.JSONtoObject(chunk);
+        });
+
+        req.on("end", async () => {
+          const { gameCenterId, playstationId } = JSON.parse(requestBody);
+
+          let gameCenterData: any;
+          let playstationData: any;
+
+          await connection
+            .select("SELECT * FROM lokasi WHERE id_loc=? ", [gameCenterId])
+            .then((chunk) => {
+              gameCenterData = chunk;
+            });
+
+          await connection
+            .select(
+              "SELECT ps.id_ps, ps.nama_ps, ps.lok, ps.status, jenis.nama_jenis, ps.harga FROM ps JOIN jenis ON ps.jenis = jenis.id_jenis WHERE ps.id_ps = ?; ",
+              [playstationId]
+            )
+            .then((chunk) => {
+              playstationData = chunk;
+            });
+
+          const data = {
+            locationId: gameCenterData.id_loc,
+            locationName: gameCenterData.nama_loc,
+            locationAddress: gameCenterData.address,
+            playstationId: playstationData.id_ps,
+            playstationName: playstationData.nama_ps,
+            playstationType: playstationData.nama_jenis,
+            playstaionPrice: playstationData.harga,
+          };
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(RestAPIFormat.status200(data)));
+        });
+      } else {
+        res.writeHead(401);
+        res.end(JSON.stringify(RestAPIFormat.status401("Unauthorized")));
+      }
+    } catch (error) {
+      res.writeHead(500);
+      res.end(JSON.stringify(RestAPIFormat.status500(error)));
+    }
+  }
+
+  public static async verifyOrderOnSiteData(
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
+    const token = req.headers.authorization;
+
+    try {
+      if (token) {
+        const authToken = JSON.parse(
+          AuthAccessToken.checkAccessToken(token) ?? ""
+        );
+
+        let requestBody: string;
+
+        req.on("data", async (chunk) => {
+          requestBody = ParseJSON.JSONtoObject(chunk);
+        });
+
+        req.on("end", async () => {
+          const { password } = JSON.parse(requestBody);
+
+          await connection
+            .select("SELECT * FROM user WHERE user_id = ? ", [authToken.userId])
+            .then((chunk) => {
+              let verifyPassword = bcrypt.compareSync(password, chunk.password);
+
+              if (verifyPassword) {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify(
+                    RestAPIFormat.status200(
+                      {
+                        verified: true,
+                      },
+                      "Verified"
+                    )
+                  )
+                );
+              } else {
+                res.writeHead(404, { "Content-Type": "application/json" });
+                res.end(
+                  JSON.stringify(
+                    RestAPIFormat.status401(
+                      {
+                        verified: false,
+                      },
+                      "Wrong password"
+                    )
+                  )
+                );
+              }
+            });
+        });
+      } else {
+        res.writeHead(401);
+        res.end(JSON.stringify(RestAPIFormat.status401("Unauthorized")));
+      }
+    } catch (error) {
+      res.writeHead(500);
+      res.end(JSON.stringify(RestAPIFormat.status500(error)));
+    }
+  }
+
   public static async postOrderOnSiteDataById(
     req: http.IncomingMessage,
     res: http.ServerResponse
   ) {
-    // const token = req.headers.authorization;
-    const token = true;
+    const token = req.headers.authorization;
 
-    if (token) {
-      // const authToken = JSON.parse(
-      //   AuthAccessToken.checkAccessToken(token) ?? ""
-      // );
+    try {
+      if (token) {
+        const authToken = JSON.parse(
+          AuthAccessToken.checkAccessToken(token) ?? ""
+        );
 
-      let requestBody: string;
+        let requestBody: string;
 
-      req.on("data", async (chunk) => {
-        requestBody = ParseJSON.JSONtoObject(chunk);
-      });
-      req.on("end", async () => {
-        const {
-          // userId,
-          // playtime,
-          // startPlay,
-          // endPlay,
-          // paymentMethod,
-          // playstationId,
-        } = JSON.parse(requestBody);
-
-        // const today = new Date();
-        const date = new Date();
-        const today = new Date(Date.parse(date.toString()));
-
-        let formattedDate = today.toLocaleString("id-ID", {
-          day: "numeric",
-          month: "numeric",
-          year: "2-digit",
+        req.on("data", async (chunk) => {
+          requestBody = ParseJSON.JSONtoObject(chunk);
         });
+        req.on("end", async () => {
+          const {
+            playtime,
+            startPlay,
+            endPlay,
+            paymentMethod,
+            playstationId,
+            totalAmount,
+            gameCenterId,
+          } = JSON.parse(requestBody);
 
-        const newFormattedDate: String = formattedDate.replace(/\//g, "");
-        console.log(newFormattedDate);
-        let rentalData: any;
+          const today = new Date();
+          let rentalData: any;
+          let newRentalId: string;
 
-        await connection
-          .select(
-            `SELECT MAX(id_rental) AS id FROM rental where id_rental LIKE '%${newFormattedDate}%' ORDER BY id_rental DESC`
-            // [newFormattedDate]
-          )
-          .then((chunk) => {
-            rentalData = chunk;
+          let formattedDate = today.toLocaleString("id-ID", {
+            day: "numeric",
+            month: "numeric",
+            year: "2-digit",
           });
 
-        console.log(rentalData);
-        console.log(rentalData[0]);
+          const newFormattedDate: String = formattedDate.replace(/\//g, "");
 
-        // if (rentalData[0].id) {
-        //   const oldRentalId = rentalData[0].id;
-        //   const regexp = /RT(\d{3})/;
+          await connection
+            .select(
+              `SELECT MAX(id_rental) AS id FROM rental where id_rental LIKE '%${newFormattedDate}%' ORDER BY id_rental DESC`
+              // [newFormattedDate]
+            )
+            .then((chunk) => {
+              rentalData = chunk;
+            })
+            .catch((err) => {
+              throw err;
+            });
 
-        //   const matches = oldRentalId.match(regexp);
-        //   const number = matches[1];
-        //   let getIntId = parseInt(number);
+          if (rentalData.id) {
+            const oldRentalId = rentalData.id;
+            const regexp = /RT(\d{3})/;
 
-        //   const rentalId = `TR${++getIntId}-${newFormattedDate}`;
+            const matches = oldRentalId.match(regexp);
+            const number = matches[1];
+            let getIntId = parseInt(number);
 
-        //   res.writeHead(200, { "Content-Type": "application/json" });
-        //   res.end(JSON.stringify(RestAPIFormat.status200({ rentalId })));
-        // } else {
-        //   const rentalId = `TR001-${newFormattedDate}`;
-        //   const rentalData = {
-        //     rentalId: rentalId,
-        //     userId: 1,
-        //     playtime: 1,
-        //     startPlay: 1,
-        //     endPlay: 1,
-        //     paymentMethod: 1,
-        //     playstationId: 1,
-        //     date: 1
-        //   }
+            newRentalId = `RT00${getIntId + 1}-${newFormattedDate}`;
+          } else {
+            newRentalId = `RT001-${newFormattedDate}`;
+          }
 
-        //   await connection
-        //     .insert(`INSERT INTO `)
+          await connection
+            .insert(
+              `INSERT INTO rental(id_rental, id_ps, waktu_order, playtime, mulai_rental, selesai_rental, bayar, lok, id_user) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+              [
+                newRentalId,
+                playstationId,
+                today,
+                playtime,
+                startPlay,
+                endPlay,
+                totalAmount,
+                gameCenterId,
+                authToken.userId,
+              ]
+            )
+            .catch((error) => {
+              throw error;
+            });
 
-        if (rentalData[0]) {
-          const oldRentalId = rentalData[0].id;
-          const regexp = /RT(\d{3})/;
-
-          const matches = oldRentalId.match(regexp);
-          const number = matches[1];
-          let getIntId = parseInt(number);
-
-          const rentalId = `TR${++getIntId}-${newFormattedDate}`;
+          if (paymentMethod == "Saldo") {
+            await connection
+              .update(`UPDATE user SET saldo = saldo - ? WHERE user_id = ?`, [
+                totalAmount,
+                authToken.userId,
+              ])
+              .catch((error) => {
+                throw error;
+              });
+          }
 
           res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(RestAPIFormat.status200({ rentalId })));
-        } else {
-          const rentalId = `TR001-${newFormattedDate}`;
+          res.end(
+            JSON.stringify(
+              RestAPIFormat.status200(
+                {
+                  rentalId: newRentalId,
+                },
+                "Order success"
+              )
+            )
+          );
+        });
+      } else {
+        res.writeHead(401);
+        res.end(JSON.stringify(RestAPIFormat.status401("Unauthorized")));
+      }
+    } catch (error) {
+      res.writeHead(500);
+      res.end(JSON.stringify(RestAPIFormat.status500(error)));
+    }
+  }
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(RestAPIFormat.status200({ rentalId })));
-        }
-      });
+  public static async getOrderOnSiteDataById(
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
+    const token = req.headers.authorization;
+
+    try {
+      if (token) {
+        const authToken = JSON.parse(
+          AuthAccessToken.checkAccessToken(token) ?? ""
+        );
+
+        let requestBody: string;
+
+        req.on("data", async (chunk) => {
+          requestBody = ParseJSON.JSONtoObject(chunk);
+        });
+        req.on("end", async () => {
+          const { rentalId } = JSON.parse(requestBody);
+
+          await connection
+            .select(
+              `SELECT rental.id_rental, rental.bayar, lokasi.nama_loc, lokasi.id_loc, jenis.nama_jenis, ps.id_ps, rental.playtime, rental.mulai_rental, user.user_id, user.email, user.username, user.hp FROM rental JOIN lokasi ON rental.lok = lokasi.id_loc JOIN ps ON rental.id_ps = ps.id_ps JOIN jenis ON ps.jenis = jenis.id_jenis JOIN user ON rental.id_user = user.user_id WHERE rental.id_rental = ? AND id_user = ?`,
+              [rentalId, authToken.userId]
+            )
+            .then((chunk) => {
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(
+                JSON.stringify(RestAPIFormat.status200(chunk, "Data found"))
+              );
+            })
+            .catch((error) => {
+              throw error;
+            });
+        });
+      } else {
+        res.writeHead(401);
+        res.end(JSON.stringify(RestAPIFormat.status401("Unauthorized")));
+      }
+    } catch (error) {
+      res.writeHead(500);
+      res.end(JSON.stringify(RestAPIFormat.status500(error)));
     }
   }
 }
